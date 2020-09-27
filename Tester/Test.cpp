e4254e9d125ff989e318bb8b1a27172a247b1a04
Steven H Ayres <steve_ayres@msn.com>
2020-09-27 13:10:03 +0100

    27/09/2020 13:10

--- Tester/Test.cpp
+++ Tester/Test.cpp
@@ -9,8 +9,15 @@ int main() {
 	ZeroMemory(&msg, sizeof(msg));
 
 	LoggerInit();
-	LogToConsole(1, "TESTER", "TEST");
-	
+	LogToConsole(LOG_INFO, "TESTER", "CONSOLE TEST");
+	LogToConsole(LOG_WARN, "TESTER", "CONSOLE TEST");
+	LogToConsole(LOG_TRACE, "TESTER", "CONSOLE TEST");
+	LogToConsole(LOG_EVENT, "TESTER", "CONSOLE TEST");
+	LogToFile(true, LOG_INFO, "TESTER", "FILE TEST");
+	LogToFile(true, LOG_WARN, "TESTER", "FILE TEST");
+	LogToFile(true, LOG_TRACE, "TESTER", "FILE TEST");
+	LogToFile(true, LOG_EVENT, "TESTER", "FILE TEST");
+
 	while (!GetExit()) {
 		
 		if (PeekMessage(&msg, NULL, NULL, NULL, PM_REMOVE))
